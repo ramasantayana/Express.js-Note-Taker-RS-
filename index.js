5eb093ad6c7e49875cf6e53b8818a0f08fccda48
ramasantayana <rama.santayana@uconn.edu>
2023-04-27 06:20:12 -0400

    Express server added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+
+
+const server = express();
+const port = process.env.PORT || 3001;
+const pDir = path.join(__dirname, "/public");
+
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
