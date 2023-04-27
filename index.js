2a913d14f330027e105c299ebd43ffcf9f35e251
ramasantayana <rama.santayana@uconn.edu>
2023-04-27 06:21:44 -0400

    Added to make server listen on the port

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ const pDir = path.join(__dirname, "/public");
 server.use(express.static("public"));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+
+server.listen(port, function () {
+  console.log(`Server listening on port ${port} to service requests from front end!`);
+});
